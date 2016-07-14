71bada502e0f357b79eab28d019a90cb2901f4b0
hvpulok <hvpulok@gmail.com>
2016-07-14 13:22:43 -0600

    Added few more Movie instances

--- movies_definitions.py
+++ movies_definitions.py
@@ -13,5 +13,28 @@ the_fountain = video.Movie("The Fountain",
                         "http://ia.media-imdb.com/images/M/MV5BMTU5OTczMTcxMV5BMl5BanBnXkFtZTcwNDg3MTEzMw@@._V1_SX640_SY720_.jpg",
                         "https://youtu.be/dAuxryJ6pv8")
 
-print the_fountain.storyline
+avatar = video.Movie("Avatar",
+                   "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+                   "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_.jpg",
+                   "https://youtu.be/d1_JBMrrYw8")
+
+school_of_rock = video.Movie("School of Rock",
+                   "After being kicked out of a rock band, Dewey Finn becomes a substitute teacher of a strict elementary private school, only to try and turn it into a rock band.",
+                   "http://ia.media-imdb.com/images/M/MV5BMjEwOTMzNjYzMl5BMl5BanBnXkFtZTcwNjczMTQyMQ@@._V1_.jpg",
+                   "https://youtu.be/XCwy6lW5Ixc")
+
+ratatouille = video.Movie("Ratatouille",
+                   "A rat who can cook makes an unusual alliance with a young kitchen worker at a famous restaurant.",
+                   "http://ia.media-imdb.com/images/M/MV5BMTMzODU0NTkxMF5BMl5BanBnXkFtZTcwMjQ4MzMzMw@@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+                   "https://youtu.be/c3sBBRxDAqk")
+
+midnigh_in_paris = video.Movie("Midnight in Paris",
+                   "While on a trip to Paris with his fiances family, a nostalgic screenwriter finds himself mysteriously going back to the 1920s everyday at midnight.",
+                   "http://ia.media-imdb.com/images/M/MV5BMTM4NjY1MDQwMl5BMl5BanBnXkFtZTcwNTI3Njg3NA@@._V1_SY1000_CR0,0,677,1000_AL_.jpg",
+                   "https://youtu.be/BYRWfS2s2v4")
+
+hunger_games = video.Movie("The Hunger Games",
+                   "Katniss Everdeen voluntarily takes her younger sister's place in the Hunger Games, a televised competition in which two teenagers from each of the twelve Districts of Panem are chosen at random to fight to the death.",
+                   "http://ia.media-imdb.com/images/M/MV5BMjA4NDg3NzYxMF5BMl5BanBnXkFtZTcwNTgyNzkyNw@@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+                   "https://youtu.be/mfmrPu43DF8")
 
